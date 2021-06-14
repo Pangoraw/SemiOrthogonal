b356c7964e2fcb0da6cb2df7e4e957c3d7476c78
Paul <paul.berg@univ-ubs.fr>
2021-06-14 12:04:06 +0200

    refactor embedding backbones

--- semi_orthogonal/backbones/__init__.py
+++ semi_orthogonal/backbones/__init__.py
@@ -1,4 +1,3 @@
-from .resnet18 import ResNet18
-from .wide_resnet50 import WideResNet50
+from .base import ResNet18, ResNet50, WideResNet50
 
-__all__ = ["WideResNet50", "ResNet18"]
+__all__ = ["WideResNet50", "ResNet50", "ResNet18"]
